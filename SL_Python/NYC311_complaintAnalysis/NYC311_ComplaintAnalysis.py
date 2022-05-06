1c0df43d5a11109085bc1a4c8a04d1357b93adda
dtthomas <dtthomas@ucalgary.ca>
2022-05-06 13:44:26 -0600

    Starting Project-NYC 311 complaint Analysis

---
+++ SL_Python/NYC311_complaintAnalysis/NYC311_ComplaintAnalysis.py
@@ -0,0 +1,5 @@
+####NYC 311 complaint Analysis#####
+###Import a 311 NYC service request.
+import pandas as pd
+import numpy as np
+data = pd.read_csv('311_Service_Requests_from_2010_to_Present.csv')
